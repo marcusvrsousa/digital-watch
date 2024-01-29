e0368c4457e23a5588b4b8b8520fb1620d50aaa0
Marcus Rocha <32283039+marcusvrsousa@users.noreply.github.com>
2024-01-28 22:52:15 -0300

    textContent modified

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ function watch() {
 
   hour.textContent =
     date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
-  //   hour.textContent = date.getHours();
-  minutes.textContent = date.getMinutes();
-  seconds.textContent = date.getSeconds();
+  minutes.textContent =
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  seconds.textContent =
+    date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
 }
 
 setInterval(watch, 1);
